5f8baaa91900300c89b5f0832afdcc4b88936616
Marvellous-Udoye <155150860+Marvellous-Udoye@users.noreply.github.com>
2024-08-09 15:02:22 +0100

    feat: "completed links component"

--- src/app/components/CustomizeLinks/CustomizeLinks.tsx
+++ src/app/components/CustomizeLinks/CustomizeLinks.tsx
@@ -1,23 +1,34 @@
 import Image from "next/image";
 import styles from "./Customize.module.css";
+import phone from "../../../../public/images/Group 273.svg"
 
 export default function CustomizeLinks() {
   return (
-    <div className="max-w-[831px] bg-[#FAFAFA] pr-6 pb-6 ">
+    <div className="w-full bg-[#FAFAFA] px-6 pb-6 ">
       <section className={styles.customize_ctn}>
         <div className={styles.custom_head}>
-          <p className="text-[32px] font-[700] ">Customize your links</p>
-            <p className="text-[16px] font-[400] text-[#737373">Add/edit/remove links below and then share all your profiles with the world!</p>
+          <p className="text-[32px] font-[700] leading-[48px] mb-[8px]">Customize your links</p>
+          <p className="text-[16px] font-[400] text-[#737373]">Add/edit/remove links below and then share all your profiles with the world!</p>
         </div>
-        <div className="grid column gap-[24px]">
-          <button className="w-full text-[] font-[] border-[1px] border-[] rounded-[] focus:outline-none hover:bg-[] ">+ Add new link</button>
+        <div className="grid grid-cols-1 gap-[24px]">
+          <button className="w-full text-[#633CFF] text-[16px] font-[600] py-[11px] px-[27px] border-[1.5px] border-[#633CFF] rounded-[8px] focus:outline-none hover:bg-[#EFEBFF] transform transition ease duration-100 ">+ Add new link</button>
           <div className={styles.Links}>
-
+            <Image
+              src={phone}
+              alt="Phone"
+              width={249.53}
+              height={160}
+              className="mx-auto"
+            />
+            <div className="flex flex-col gap-[24px]">
+              <p className="text-[32px] font-[700] text-center">Let's get you started</p>
+              <p className="text-[16px] font-[400] text-center text-[#737373] max-w-[488px] mx-auto">Use the “Add new link” button to get started. Once you have more than one link, you can reorder and edit them. We’re here to help you share your profiles with everyone!</p>
+            </div>
           </div>
         </div>
       </section>
-      <section>
-        <button>Save</button>
+      <section className="py-[24px] px-[40px] flex justify-end border-t-[1px] border-t-[#D9D9D9] bg-[#fff] ">
+        <button className={styles.save_btn}>Save</button>
       </section>
     </div>
   );
--- src/app/components/Displaylinks/DisplayLink.tsx
+++ src/app/components/Displaylinks/DisplayLink.tsx
@@ -3,7 +3,7 @@ import styles from "./DisplayLink.module.css";
 
 export default function DisplayLink() {
     return (
-        <div className="max-w-[608px] bg-[#FAFAFA] px-6 pb-6 ">
+        <div className={`${styles.display_ctn_holder} max-w-[608px] bg-[#FAFAFA] pl-6 pb-6 `}>
             <aside className={styles.display_ctn}>
                 {/* <div className="border-[1px] border-[#737373] rounded-10"></div>
                 <div className="border-[1px] border-[#737373] rounded-10 my-[10px] mx-[11px]"></div> */}
