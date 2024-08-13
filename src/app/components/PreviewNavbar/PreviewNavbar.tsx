b61bf1e1421e755e9aa7e6f4ba322e396bee8c48
Marvellous-Udoye <155150860+Marvellous-Udoye@users.noreply.github.com>
2024-08-13 17:05:14 +0100

    layout: 'edited previewnavbar component'

--- src/app/components/PreviewNavbar/PreviewNavbar.tsx
+++ src/app/components/PreviewNavbar/PreviewNavbar.tsx
@@ -25,16 +25,16 @@ export default function Preview() {
 
   return (
     <div>
-      <nav className={styles.preview_navbar}>
+      <nav className="flex justify-between p-6 bg-[#633CFF]">
         <div className="flex items-center justify-between rounded-[12px] px-6 py-4 bg-[#fff] w-full">
           <Link href='/HomePage'>
-            <button className={`${styles.preview_btn} hover:bg-[#EFEBFF] text-[#633CFF]`}>
+            <button className={`py-[11px] px-[27px] rounded-[8px] border-[1px] border-[#633CFF] text-[16px] font-[600] leading-[24px] transform transition ease-in-out duration-100 hover:bg-[#EFEBFF] text-[#633CFF]`}>
               Back to Editor
             </button>
           </Link>
           <button
             onClick={handleShare}
-            className={`${styles.preview_btn} bg-[#633CFF] text-[#fff]`}>
+            className={`py-[11px] px-[27px] rounded-[8px] border-[1px] border-[#633CFF] text-[16px] font-[600] leading-[24px] transform transition ease-in-out duration-100 bg-[#633CFF] text-[#fff]`}>
             Share Link
           </button>
         </div>
