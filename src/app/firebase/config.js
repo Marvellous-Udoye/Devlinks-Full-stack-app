3b518f6c1980b2b094683805d8ce18469fd7de71
Marvellous-Udoye <155150860+Marvellous-Udoye@users.noreply.github.com>
2024-09-25 04:55:56 +0100

    feat: implement database and store create account details

---
+++ src/app/firebase/config.js
@@ -0,0 +1,17 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_apiKey,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_authDomain,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_projectId,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_storageBucket,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_messagingSenderId,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_appId,
+};
+
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig)
+
+const auth = getAuth(app)
+
+export { app, auth }
