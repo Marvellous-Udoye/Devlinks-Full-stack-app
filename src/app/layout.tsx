cacf7cd3cc01bde0243842066933e5bd23e71368
Marv_lous <155150860+Marvellous-Udoye@users.noreply.github.com>
2024-10-16 19:05:14 +0100

    chore: change metadata description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
 "use client"
 
+import { metadata } from "./metadata";
 import { Inter } from "next/font/google";
-import "./globals.css";
-import SessionProvider from "./SessionProvider";
 import { Provider } from "react-redux";
+import "./globals.css";
 import store from "./redux/store";
+import SessionProvider from "./SessionProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,9 +14,15 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const title = String(metadata?.title || "Default Title");
+  const description = metadata?.description || "Default description";
+
   return (
     <html lang="en">
       <head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
         <link rel="icon" type="image/svg" href="/images/logo.svg" />
       </head>
       <body className={inter.className}>
